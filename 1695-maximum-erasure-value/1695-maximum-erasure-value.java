1d11f8c71648a6b06885947eb6053fbf658c991e
ABHIK01 <abhiksamanta01@gmail.com>
2022-06-12 11:51:33 +0530

    Time: 136 ms (21.93%), Space: 120.9 MB (34.65%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.java
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.java
@@ -3,9 +3,10 @@ class Solution {
         HashSet<Integer> set = new HashSet<>();
         int length = nums.length;
         int sum = 0;
-        int end = 0;
+        //int end = 0;
         int start = 0;
         int maxSum = 0;
+        /*
         while(end < length) {
             if(set.add(nums[end])){
                 sum += nums[end];
@@ -17,6 +18,19 @@ class Solution {
             }
             maxSum = Math.max(maxSum,sum);
             
+        }
+        
+        return maxSum;
+        */
+        for(int end = 0;end<length;end++) {
+            while(set.contains(nums[end])){
+                sum -= nums[start];
+                set.remove(nums[start]);
+                start++;
+            }
+            sum += nums[end];
+            set.add(nums[end]);
+            maxSum = Math.max(maxSum,sum);
         }
         
         return maxSum;

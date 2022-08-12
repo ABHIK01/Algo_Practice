39178c3d5cb877a633b6ede14fe869ab151d8703
ABHIK01 <abhiksamanta01@gmail.com>
2022-08-12 10:35:19 +0530

    Time: 1 ms (62.14%), Space: 42.1 MB (88.84%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -16,22 +16,18 @@
 class Solution {
     public boolean isValidBST(TreeNode root) {
         List<Integer> list = new ArrayList<>();
-        
         dfs(root,list);
-        
-        for(int i = 0;i<list.size() - 1;i++) {
+        for(int i = 0;i<list.size()-1;i++) {
             if(list.get(i+1) <= list.get(i)){
                 return false;
             }
         }
-        
         return true;
         
     }
-    
     public void dfs(TreeNode root,List<Integer> list) {
         if(root != null) {
-            dfs(root.left,list);
+           dfs(root.left,list);
             list.add(root.val);
             dfs(root.right,list);
         }

e7c7efaa85e933a24be712fe225811f4c638a7b1
ABHIK01 <abhiksamanta01@gmail.com>
2022-08-14 10:51:05 +0530

    Time: 3 ms (38.20%), Space: 41.6 MB (29.23%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x < 2 ) {
+            return x;
+        }
+        long num;
+        int left = 2,right = x/2;
+        while(left <= right) {
+            int mid = left + (right-left)/2;
+            num = (long)mid * mid;
+            if(num == x) {
+                return mid;
+            } else if(num > x) {
+                right = mid-1;
+            } else {
+                left = mid+1;
+            }
+        }
+        return right;
+        
+    }
+}

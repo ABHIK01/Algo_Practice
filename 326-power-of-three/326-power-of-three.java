5bb4420b4a4b85c91304b1817ad7942f1f79a4af
ABHIK01 <abhiksamanta01@gmail.com>
2022-08-24 22:16:05 +0530

    Time: 24 ms (45.30%), Space: 46.9 MB (74.59%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n == 0) {
+            return false;
+        }
+        while(n != 1) {
+            int remainder = n%3;
+            if(remainder != 0) {
+                return false;
+            }
+            n = n/3;
+        }
+        return true;
+        
+    }
+}

73de20f8895c36f47420e628338d76bdb26fd816
ABHIK01 <abhiksamanta01@gmail.com>
2022-08-27 20:31:34 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (7.33%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -36,7 +36,7 @@ class Solution {
         
         return Math.max(inc,exc);
         */
-        
+        /*
         int inc = nums[0];
         int exc = 0;
         for(int i = 1;i<nums.length;i++) {
@@ -47,6 +47,20 @@ class Solution {
         }
         
         return Math.max(inc,exc);
+        */
+        int[] dp = new int[nums.length];
+        if(nums.length == 1){
+            return nums[0];
+        } else {
+            dp[0] = nums[0];
+            dp[1] = Math.max(nums[0],nums[1]);
+            
+            for(int i = 2;i<nums.length;i++) {
+                dp[i] = Math.max(dp[i-1],nums[i] + dp[i-2] );
+            }
+        }
+        
+        return dp[nums.length-1];
         
         
     }

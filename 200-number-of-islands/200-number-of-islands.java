f2b912273fe168492311688a7c2be98961eca8a4
ABHIK01 <abhiksamanta01@gmail.com>
2022-08-30 09:09:07 +0530

    Time: 2 ms (100.00%), Space: 50.7 MB (93.77%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -6,22 +6,25 @@ class Solution {
         for(int i = 0;i<row;i++) {
             for(int j = 0;j<column;j++) {
                 if(grid[i][j] == '1'){
-                    dfs(grid,i,j,row,column);
+                    dfs(i,j,row,column,grid);
                     count++;
                 }
             }
         }
-       return count;
+        
+        return count;
+        
     }
-    void  dfs(char[][] grid,int i,int j,int row,int column) {
-           if( i < 0 || j < 0 || i >= row || j >= column || grid[i][j] == '0'){
-               return;
-           }
-           grid[i][j] = '0';
-           dfs( grid, i+1, j, row, column);
-           dfs( grid, i-1, j, row, column);
-           dfs( grid, i, j+1, row, column);
-           dfs( grid, i, j-1, row, column);
-           
-       }
+    
+    void dfs(int i,int j,int row,int column,char[][] grid) {
+        if(i< 0 || j< 0|| i>= row  || j>= column || grid[i][j]  == '0' ){
+            return ;
+        }
+        
+        grid[i][j] = '0';
+        dfs(i+1,j,row,column,grid);
+        dfs(i-1,j,row,column,grid);
+        dfs(i,j+1,row,column,grid);
+        dfs(i,j-1,row,column,grid);
+    }
 }

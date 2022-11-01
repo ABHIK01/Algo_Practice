bdc604f857affca20a8b0d0ce2f71fe5781ea2a2
ABHIK01 <abhiksamanta01@gmail.com>
2022-11-01 22:06:34 +0530

    Time: 52 ms (18.78%), Space: 75.5 MB (15.10%) - LeetHub

---
+++ 1198-find-smallest-common-element-in-all-rows/1198-find-smallest-common-element-in-all-rows.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int smallestCommonElement(int[][] mat) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int row = mat.length;
+        int column = mat[0].length;
+        for(int i = 0;i<row;i++){
+            for(int j = 0;j<column;j++){
+                int val = mat[i][j];
+                map.put(val,map.getOrDefault(val,0)+1);
+            }
+        }
+        
+        int smallestValue = Integer.MAX_VALUE;
+        for(int value :map.keySet()){
+            if(map.get(value) == row && value < smallestValue ){
+                smallestValue = value;
+            }
+        }
+        
+        return smallestValue != Integer.MAX_VALUE ? smallestValue  : -1;
+    }
+}
